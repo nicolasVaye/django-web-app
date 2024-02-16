e3ad691153ce86c9c7469251e6176c6dcb114daf
nicolas Vaye <nicolas.vaye@u-bordeaux.fr>
2024-02-16 11:41:17 +0100

    avant gérez des données à l'aide du site d'admin de django

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,4 +1,22 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
-# NICO test
+from listings.models import Band, Title
+
+
+def hello(request):
+    bands = Band.objects.all()
+    return render(request, 'listings/hello.html', {'bands': bands})
+
+
+def about(request):
+    return render(request, 'listings/about.html')
+
+
+def listings(request):
+    titles = Title.objects.all()
+    return render(request, 'listings/listings.html', {'titles': titles})
+
+
+def contact_us(request):
+    return render(request, 'listings/contact_us.html')
